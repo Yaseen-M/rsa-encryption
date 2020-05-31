28f7e105abf40bcbb6d13a8a4fa0382b1ea169c5
Mohammed Yaseen <mohammedysn15@gmail.com>
2020-05-31 17:15:22 +0100

    Create prime functions

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import random
+import math
+
+
 def hcf(a, b):
     while b != 0:
         a, b = b, a % b
@@ -8,13 +12,43 @@ def is_comprime(a, b):
     return hcf(a, b) == 1
 
 
+def is_prime(n):
+    if n > 1:
+        for i in range(2, n):
+            if (n % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def generate_prime(lower, upper):
+    while True:
+        n = random.randint(lower, upper)
+        if is_prime(n):
+            return n
+
+
+def generate_p_q():
+    p = generate_prime(100, 10000)
+    while True:
+        q = generate_prime(100, 10000)
+        if p != q:
+            break
+    return p, q
+
+
 def main():
-    p = 8288669
-    q = 2047933
+    p, q = generate_p_q()
+
+    # n is the modulus for the pub and priv keys
     n = p * q
 
     totient = (p - 1) * (q - 1)
-    print(is_comprime(totient, 5))
+    while True:
+        e = random.randint(2, totient - 1)
+        if is_comprime(e, totient):
+            break
 
-
-main()
+    d = (1 % totient) / e
